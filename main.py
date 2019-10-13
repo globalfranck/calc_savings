92b97c4d713237034cc3e5482cc0a03bdca59e9c
globalfranck <50407517+globalfranck@users.noreply.github.com>
2019-10-13 20:24:55 +0200

    Adding spendings

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ income_per_year = weeks_per_year * income_per_week
 
 print(name + "'s yearly income is:")
 print(income_per_year)
+
+spend_per_week = 400
+spend_per_year = spend_per_week * 52
+print(name + "'s yearly spend is:")
+print(spend_per_year)
